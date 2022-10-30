6e610f2494c8bc5b3a5de7984e6e296aa50ffc83
pbxx <airlew@gmail.com>
2022-10-29 23:23:34 -0600

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+const path = require("path");
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: path.resolve(__dirname, "./dist/"),
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+})
